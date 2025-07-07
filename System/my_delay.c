10bf58f588d9b5d85d00bfa70207b18e8b7b77c6
proud king <3213626955@qq.com>
2025-07-08 00:58:05 +0800

    System移植——新增delay

--- System/my_delay.c
+++ System/my_delay.c
@@ -1,10 +1,12 @@
 #include "my_delay.h"
 
+//实现微秒级延时
 void delay_us(uint32_t i)
 {
-   
+   delay_cycles((i/1000000)*CPUCLK_FREQ);
 }
 
+//实现毫秒级延时
 void delay_ms(uint32_t nms)
 {
     for (uint32_t i = 0; i < nms; i++) {
