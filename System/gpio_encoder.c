f7974f37c15a8071f0b7079d677e062ba31a488c
proud king <3213626955@qq.com>
2025-07-08 15:01:21 +0800

    System移植——新增gpio_encoder

---
+++ System/gpio_encoder.c
@@ -0,0 +1,86 @@
+#include "encoder.h"
+
+//定义编码器数量
+#define encoder_num 2
+ENCODER_PORT encoder[encoder_num];
+
+void encoder_init(int encoder_id,ENCODER_PORT encoder_port){
+      encoder[encoder_id] = encoder_port;
+}
+
+void encoder_start(int encoder_id){
+	 NVIC_EnableIRQ(encoder[encoder_id].IRQn);
+}
+
+int32_t encoder_getcounter(int encoder_id){
+
+    int32_t counter = 0;
+    counter = encoder[encoder_id].encoder_counter;
+    encoder[encoder_id].encoder_counter = 0;
+    return counter;
+}
+
+void GROUP1_IRQHandler(void){
+
+  for(int i = 0; i < encoder_num; i++){
+uint32_t encoder_itstatus = DL_GPIO_getEnabledInterruptStatus(encoder[i].gpio_port, encoder[i].EncoderA_PIN| encoder[i].EncoderB_PIN| encoder[i].EncoderC_PIN| encoder[i].EncoderD_PIN);
+
+
+   if(encoder_itstatus & encoder[i].EncoderA_PIN)
+   {
+     if(DL_GPIO_readPins(encoder[i].gpio_port,encoder[i].EncoderB_PIN))
+     {
+         encoder[i].encoder_counter++;
+     }
+     else
+     {
+         encoder[i].encoder_counter--;
+     }
+     DL_GPIO_clearInterruptStatus(encoder[i].gpio_port,encoder[i].EncoderA_PIN);
+   }
+
+
+   else if(encoder_itstatus & encoder[i].EncoderC_PIN)
+   {
+     if(DL_GPIO_readPins(encoder[i].gpio_port,encoder[i].EncoderB_PIN))
+     {
+         encoder[i].encoder_counter--;
+     }
+     else
+     {
+         encoder[i].encoder_counter++;
+     }
+     DL_GPIO_clearInterruptStatus(encoder[i].gpio_port,encoder[i].EncoderC_PIN);
+   }
+
+
+   else if(encoder_itstatus & encoder[i].EncoderB_PIN)
+   {
+     if(DL_GPIO_readPins(encoder[i].gpio_port,encoder[i].EncoderA_PIN))
+     {
+        encoder[i].encoder_counter--;
+     }
+     else
+     {
+        encoder[i].encoder_counter++;
+     }
+     DL_GPIO_clearInterruptStatus(encoder[i].gpio_port,encoder[i].EncoderB_PIN);
+   }
+
+
+   else if(encoder_itstatus & encoder[i].EncoderD_PIN)
+   {
+     if(DL_GPIO_readPins(encoder[i].gpio_port,encoder[i].EncoderA_PIN))
+     {
+        encoder[i].encoder_counter++;
+     }
+     else
+     {
+        encoder[i].encoder_counter--;
+     }
+     DL_GPIO_clearInterruptStatus(encoder[i].gpio_port,encoder[i].EncoderD_PIN);
+   }
+
+  
+   }
+}
---
+++ System/gpio_encoder.h
@@ -0,0 +1,32 @@
+#ifndef _ENCODER_H
+#define _ENCODER_H
+
+#include <ti/driverlib/dl_gpio.h>
+
+typedef struct
+{
+//中断线
+    IRQn_Type IRQn;
+
+//GPIO端口
+    GPIO_Regs *gpio_port;
+
+//编码器引脚
+    uint32_t EncoderA_PIN;
+    uint32_t EncoderB_PIN;
+    uint32_t EncoderC_PIN;
+    uint32_t EncoderD_PIN;
+
+//编码器计数器
+    int32_t encoder_counter;
+
+//编码器ID
+    int encoder_id;
+
+}ENCODER_PORT;
+
+void encoder_init(int encoder_id, ENCODER_PORT encoder_port);
+void encoder_start(int encoder_id);
+int32_t encoder_getcounter(int encoder_id);
+
+#endif 
