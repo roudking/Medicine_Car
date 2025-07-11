5805a36983ba9e6d255f3c430b7fe47456293ec2
proud king <3213626955@qq.com>
2025-07-11 23:26:16 +0800

    完成主车速度环PID 和 角度环PID 参数整定

--- Mask/Mask.c
+++ Mask/Mask.c
@@ -4,27 +4,27 @@
 CAR car;
 //已整定pid
 PID pidL = {
-    .kp = 155.5,
-    .ki =36.0,
+    .kp = 170.5,
+    .ki =43.0,
     .kd = 0.0
 };
 PID pidR = {
-    .kp = 156.5,
-    .ki = 40.0,
+    .kp = 180.5,
+    .ki =43.0,
     .kd = 0.0
 };
 
 PID pidtrance = {
-    .kp = 0.83,
+    .kp = 0.75,
     .ki = 0.00,
-    .kd = 1.00,
+    .kd = 2.77,
     .out_xianfu = 15.0
 };
 
 PID pidturn = {
-    .kp = 0.83,
+    .kp = 0.75,
     .ki = 0.00,
-    .kd = 1.00,
+    .kd = 2.77,
     .out_xianfu = 15.0
 };
 
@@ -89,75 +89,24 @@ void Mask_Timer_INST_IRQHandler(void)
     // //获取目标速度
     //  Car_gettargetspeed(&car);
       
-    //  float delta;
-    //  if(car.state.turn_state == 1){
-    //    delta = Car_turnpidcal(&car);
-    //  //判定转弯完成
-    //    if(fabs(car.imu.real_yaw - car.imu.zero_yaw) < 2.0){  
-    //       car.state.turn_state = 0;
-    //    }
-    //  }
-    //  else if(car.state.turn_state == 0){
-    //    delta = Car_trancepidcal(&car);
-    //  }
+     float delta;
+     if(car.state.turn_state == 1){
+       delta = Car_turnpidcal(&car);
+     //判定转弯完成
+       if(fabs(car.imu.real_yaw - car.imu.zero_yaw) < 2.0){  
+          car.state.turn_state = 0;
+       }
+     }
+     else if(car.state.turn_state == 0){
+       delta = Car_trancepidcal(&car);
+     }
 
-    //  Driver_setmotor_targetspeed(&(car.motor1), car.raspberry.leftspeed - delta);
-    //  Driver_setmotor_targetspeed(&(car.motor2), car.raspberry.rightspeed + delta);
+     Driver_setmotor_targetspeed(&(car.motor1), car.raspberry.leftspeed - delta);
+     Driver_setmotor_targetspeed(&(car.motor2), car.raspberry.rightspeed + delta);
 
 
-    // TEST代码
-	int time = 1500;
-	static int i = 0;
-  if(i<time/10)
-	{
-    Driver_setmotor_targetspeed(&(car.motor2),0.0);
-    Driver_setmotor_targetspeed(&(car.motor1),0.0);
-		i++;
-	}
-	else if(i < 2*time/10)
-	{
-    Driver_setmotor_targetspeed(&(car.motor2),25.0);
-    Driver_setmotor_targetspeed(&(car.motor1),25.0);
-		i++;	
-	}
-	else if(i < 3*time/10)
-	{
-    Driver_setmotor_targetspeed(&(car.motor1),40.0);
-    Driver_setmotor_targetspeed(&(car.motor2),40.0);
-		i++;	
-	}    
-	else if(i < 4*time/10)
-	{
-    Driver_setmotor_targetspeed(&(car.motor1),25.0);
-    Driver_setmotor_targetspeed(&(car.motor2),25.0);
-		i++;
-	}
-    else if(i < 5*time/10){
-    Driver_setmotor_targetspeed(&(car.motor1),0.0);  
-    Driver_setmotor_targetspeed(&(car.motor2),0.0);
-    i++;
-    }
-  else if(i < 6*time/10){
-    Driver_setmotor_targetspeed(&(car.motor1),-25.0);  
-    Driver_setmotor_targetspeed(&(car.motor2),-25.0);
-    i++;
-    }
-  else if(i < 7*time/10){
-    Driver_setmotor_targetspeed(&(car.motor1),-40.0);  
-    Driver_setmotor_targetspeed(&(car.motor2),-40.0);
-    i++;
-    }
-  else if(i < 8*time/10){
-    Driver_setmotor_targetspeed(&(car.motor1),-25.0);  
-    Driver_setmotor_targetspeed(&(car.motor2),-25.0);
-    i++;
-    }
-  else{
-    Driver_setmotor_targetspeed(&(car.motor1),0.0);  
-    Driver_setmotor_targetspeed(&(car.motor2),0.0);
-  }
 
-    Debugger_printf("%d,%d\n",car.motor1.currentspeed,car.motor2.currentspeed);
+    // Debugger_printf("%d,%d\n",car.motor1.currentspeed,car.motor2.currentspeed);
     // Debugger_printf("%.3f,%.3f,%.3f\n",car.imu.current_yaw,car.imu.real_yaw,car.imu.zero_yaw);
 
     Driver_setspeed(&(car.motor1),&(car.motor2));
