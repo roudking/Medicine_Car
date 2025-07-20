698b67035262feeb5be5e6aaae0df0b733799b75
proud king <3213626955@qq.com>
2025-07-20 18:03:51 +0800

    最终封箱版

--- Mask/Mask_perform.c
+++ Mask/Mask_perform.c
@@ -61,8 +61,7 @@ void Mask_performmasks(CAR *car)
 		}
 		else
 		{
-		 int i = Mask_performmask(car,car->mask.mask_list[car->mask.mask_pc]);
-		  car->mask.mask_pc = car->mask.mask_pc + i;
+		  car->mask.mask_pc += Mask_performmask(car,car->mask.mask_list[car->mask.mask_pc]);
 		}
 
 	 }
